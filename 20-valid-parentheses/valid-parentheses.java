0d50da53a674b026571ca22c94de0f15ddef06b6
Harshit Singh <harshitsinghh45@gmail.com>
2026-07-06 22:38:32 +0530

    Time: 3 ms (87.19%) | Memory: 43.2 MB (57.76%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(char c : s.toCharArray()){
+            if( c == '(' || c == '{' || c == '['){
+                stk.push(c);
+            }
+            else if ( c == ')' || c == '}' || c == ']' ){
+                if(stk.isEmpty()){
+                    return false;
+                }
+                char top = stk.peek();
+                if((c == ')' && top != '(' ) ||
+                   (c == '}' && top != '{' ) ||
+                   (c == ']' && top != '[' ) ){
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        return stk.isEmpty();
+    }
+}
