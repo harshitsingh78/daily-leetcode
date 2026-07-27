78703531618e0ea8c244621e63ab1a9903090a16
Harshit Singh <harshitsinghh45@gmail.com>
2026-07-27 19:51:15 +0530

    Time: 2 ms (99.31%) | Memory: 44.4 MB (65.00%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,14 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder str = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(Character.isLetterOrDigit(ch)){
-                str.append(Character.toLowerCase(ch));
+        int left =0;
+        int right = s.length()-1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++ ;
             }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right-- ;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++ ;
+            right-- ;
         }
-        String original = str.toString();
-        String reverese = str.reverse().toString();
-        return original.equals(reverese);
+        return true;
     }
 }
